4514b4b01a5abb3475aeabf698a1b55a68b89052
bczmarko <bczmarko@intrend.hu>
2023-11-15 16:09:30 +0100

    app base and welcome component implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/shortly-header/header/header.component";
+import {WelcomeComponent} from "./components/welcome/welcome.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, WelcomeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+export class WelcomeComponent {
+
+}
